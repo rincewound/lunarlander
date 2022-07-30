4a7cbabf3d86c445acf9a3f9adf2fdd7e7d90160
Marco Dahl <dahl.marco@gmail.com>
2022-07-30 14:38:23 +0200

    Add draw method for multiple lines

--- src/draw/mod.rs
+++ src/draw/mod.rs
@@ -8,7 +8,7 @@ pub trait Drawable
     fn to_point(&self) -> Point;
 }
 
-pub fn draw_line<T>(canvas: &mut Canvas<Window>, from: T, to: T, 
+pub fn draw_line<T>(canvas: &mut Canvas<Window>, from: &T, to: &T, 
     color: Color) -> Result<(), String>
     where T: Drawable
 {
@@ -16,10 +16,17 @@ pub fn draw_line<T>(canvas: &mut Canvas<Window>, from: T, to: T,
     return canvas.draw_line(from.to_point(), to.to_point());
 }
 
-//pub fn draw_lines<T>(canvas: &mut Canvas<Window>, points: &Vec<Box<T>>, color: Color)
-//    -> Result<(), String>
-//    where T: Drawable
-//{
-//    canvas.set_draw_color(color);
-//    return canvas.draw_lines::<&[sdl2::rect::Point]>(&points.iter().map(|p| { p.to_point()}).collect());
-//}
+pub fn draw_lines<T>(canvas: &mut Canvas<Window>, points: &Vec<Box<T>>, color: Color)
+    -> Result<(), String>
+    where T: Drawable
+{
+    canvas.set_draw_color(color);
+
+    for idx in 1..points.len() {
+        if let Err(error) = draw_line(canvas, &*points[idx-1], &*points[idx], color) {
+            return Err(error);
+        }
+    }
+
+    Ok(())
+}
