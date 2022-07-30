45f58d15e807756da8406ac3f3c7ccdb32160b90
rincewound <aaargh@minddebugger.com>
2022-07-30 11:01:59 +0200

    Add len method

--- src/vecmath/mod.rs
+++ src/vecmath/mod.rs
@@ -1,30 +1,41 @@
 
-pub struct vec2d
+pub struct Vec2d
 {
     pub x: f32,
     pub y: f32
 }
 
-impl vec2d
+impl Vec2d
 {
     pub fn new(x: f32, y: f32) -> Self
     {
-        vec2d{
+        Vec2d{
             x,
             y
         }
+    }
+
+    pub fn len(&self) -> f32
+    {
+        return (self.x* self.x + self.y*self.y).sqrt();
     }
 }
 
 
 #[cfg(test)]
 mod tests {
-    use crate::vecmath::vec2d;
+    use crate::vecmath::Vec2d;
 
     #[test]
     pub fn create_vec()
     {
-        let x = vec2d::new(10.0, 20.0);
+        let _x = Vec2d::new(10.0, 20.0);
     }
 
+    #[test]
+    pub fn len_works()
+    {
+        let x = Vec2d::new(1.0, 1.0);
+        assert_eq!(2.0_f32.sqrt(), x.len());
+    }
 }
