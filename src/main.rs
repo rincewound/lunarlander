f2f5ecec3c02e10075aa94614be713e3c23adf93
Marco Dahl <dahl.marco@gmail.com>
2022-07-30 15:06:57 +0200

    Extract draw example to separate function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use sdl2::render::Canvas;
+use sdl2::video::Window;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
@@ -43,15 +45,7 @@ pub fn main() -> Result<(), String> {
         canvas.set_draw_color(Color::RGB(0, 0, 0));
         canvas.clear();
 
-        let p1 = Box::new(vecmath::Vec2d::new(0.0, 0.0));
-        let p2 = Box::new(vecmath::Vec2d::new(100.0, 100.0));
-        let p3 = Box::new(vecmath::Vec2d::new(200.0, 500.0));
-
-        let mut points = Vec::new();
-        points.push(p1);
-        points.push(p2);
-        points.push(p3);
-        draw::draw_lines(&mut canvas, &points, Color::RGB(255, 255, 255)).unwrap();
+        draw_example(&mut canvas);
 
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
@@ -59,4 +53,20 @@ pub fn main() -> Result<(), String> {
     }
 
     Ok(())
+}
+
+fn draw_example(canvas: &mut Canvas<Window>) 
+{
+    let p1 = Box::new(vecmath::Vec2d::new(0.0, 0.0));
+    let p2 = Box::new(vecmath::Vec2d::new(100.0, 100.0));
+    let p3 = Box::new(vecmath::Vec2d::new(200.0, 500.0));
+
+    let mut points = Vec::new();
+    points.push(p1);
+    points.push(p2);
+    points.push(p3);
+    draw::draw_lines(canvas, &points, Color::RGB(255, 255, 255)).unwrap();
+
+    let origin = vecmath::Vec2d::new(300.0, 300.0);
+    draw::draw_rect(canvas, &origin, 50, 100, Color::RGB(0, 0, 255), true).unwrap();
 }
