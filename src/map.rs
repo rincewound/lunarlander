23f462cf0f2bcb0c05d1caf4811ee11811c03131
Hardy <hardy7cc@gmail.com>
2022-07-31 13:42:02 +0200

    Introduce landing places (set to 2 atm)

--- src/map.rs
+++ src/map.rs
@@ -10,29 +10,37 @@ pub struct PointList {
 const X_MAX_DELTA: f32 = 10.0;
 const Y_MAX_DELTA: f32 = 200.0;
 const Y_DELTA_DIVIDOR: f32 = 1.70;
-const X_START_POINTS: usize = 10;
+const X_START_POINTS: usize = 5;
+const X_LANDING_AREA_SIZE: f32 = 20.0;
 
 impl PointList {
     pub fn new(maxX: f32, maxY: f32) -> Self {
         let mut start_points: Vec<Vec2d> = Vec::new();
-        for val in (0..=(maxX as usize)).step_by((maxX as usize) / X_START_POINTS) {
-            let val = val as f32;
-            println!("Start point val: {}", val);
-            start_points.push(Vec2d::new(val, randomY(0.0, maxY)))
+        let xStepSize = maxX / (X_START_POINTS as f32);
+        assert!(xStepSize > X_LANDING_AREA_SIZE);
+        for stepIdx in 0..=X_START_POINTS {
+            start_points.push(Vec2d::new(xStepSize * (stepIdx as f32), randomY(0.0, maxY)))
         }
+        let landingPointsIdx = Self::genLandingPointsIdx(2, start_points.len());
+
         let mut gen_map = Vec::new();
 
-        gen_map.push(start_points.first().unwrap().clone());
-        for idx in 1..start_points.len() {
+        for idx in 0..(start_points.len() - 1) {
             gen_map.push(start_points[idx].clone());
+            if landingPointsIdx.contains(&idx) {
+                let secLandingPoint =
+                    gen_map.last().unwrap().clone() + Vec2d::new(X_LANDING_AREA_SIZE, 0.0);
+                gen_map.push(secLandingPoint);
+            }
             split(
-                start_points[idx - 1].clone(),
-                start_points[idx].clone(),
+                gen_map.last().unwrap().clone(),
+                start_points[idx + 1].clone(),
                 &mut gen_map,
                 X_MAX_DELTA,
                 Y_MAX_DELTA,
             );
         }
+        gen_map.push(start_points.last().unwrap().clone());
 
         let mut n = PointList {
             values: Vec::from(gen_map),
@@ -47,6 +55,13 @@ impl PointList {
 
     fn sort(self: &mut Self) {
         self.values.sort_by(|a, b| a.x.partial_cmp(&b.x).unwrap());
+    }
+
+    fn genLandingPointsIdx(numLandings: usize, numStartPoints: usize) -> Vec<usize> {
+        let mut rng = rand::thread_rng();
+        rng.sample_iter(Uniform::new(0, numStartPoints))
+            .take(numLandings)
+            .collect()
     }
 }
 
@@ -64,7 +79,7 @@ fn randomY(minValue: f32, maxValue: f32) -> f32 {
 }
 
 fn split(a: Vec2d, b: Vec2d, list: &mut Vec<Vec2d>, xMinDist: f32, yMaxDelta: f32) {
-    assert_eq!(a.x < b.x, true);
+    assert!(a.x < b.x);
     let deltaX = (b.x - a.x) / 2.0;
     let center = (a + b) / 2.0;
     let newY = randomY(center.y - (yMaxDelta / 2.0), center.y + (yMaxDelta / 2.0));
@@ -96,10 +111,18 @@ mod tests {
 
     #[test]
     fn test_list_gen() {
-        let li = PointList::new(10.0, 20.0);
+        let li = PointList::new(150.0, 20.0);
+        println!("Point list: {:?}", li);
+        assert!(li.values.first().unwrap().x == 0.0);
+        assert!(li.values.last().unwrap().x == 150.0);
+    }
+
+    #[test]
+    fn test_list_gen_2() {
+        let li = PointList::new(300.0, 20.0);
         println!("Point list: {:?}", li);
         assert_eq!(li.values.first().unwrap().x == 0.0, true);
-        assert_eq!(li.values.last().unwrap().x == 10.0, true);
+        assert_eq!(li.values.last().unwrap().x == 300.0, true);
     }
 
     #[test]
@@ -108,7 +131,7 @@ mod tests {
             Vec2d::new(0.0, randomY(0.0, 100.0)),
             Vec2d::new(100.0, randomY(0.0, 100.0)),
         ]);
-        split(list[0].clone(), list[1].clone(), &mut list, 5.0, 20.0);
+        split(list[0], list[1], &mut list, 5.0, 20.0);
         println!("Point list: {:?}", list);
     }
 
